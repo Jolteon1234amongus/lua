996fcde054478a53e2d4ef291d346e5df541d025
Jolteon1234amongus <100394766+Jolteon1234amongus@users.noreply.github.com>
2023-04-07 14:23:17 -0400

    Update custom unnamed esp [unnamed esp].lua
    --ORIGINAL: 
    --https://github.com/ic3w0lf22/Unnamed-ESP/blob/master/UnnamedESP.lua

--- custom unnamed esp [unnamed esp].lua
+++ custom unnamed esp [unnamed esp].lua
@@ -1,7 +1,3 @@
---ORIGINAL: 
---https://github.com/ic3w0lf22/Unnamed-ESP/blob/master/UnnamedESP.lua
-
-
 assert(Drawing, 'exploit not supported')
 
 if not syn and not PROTOSMASHER_LOADED then print'Unnamed ESP only officially supports Synapse and Protosmasher! If you\'re an exploit developer and have added drawing API to your exploit, try setting syn as true then checking if that works, otherwise, DM me on discord @ cppbook.org#1968 or add an issue to the Unnamed ESP Github Repository and I\'ll see it through email!' end
